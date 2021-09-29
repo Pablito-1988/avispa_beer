873f5a9413f89bdc26f58d731df8f48cdc2c3bc5
Pablo Armando <pabloar24.7@hmail.com>
2021-09-29 20:15:43 -0300

    files and folders created

--- my-app/src/components/header/header.js
+++ my-app/src/components/header/header.js
@@ -1,5 +1,6 @@
 import './header.css'
 import logo from './assets/La Avispa v1.jpg'
+import { Link } from 'react-router-dom'
 
 function Header(){
     return(
@@ -9,12 +10,10 @@ function Header(){
                    <img className='logo' src={logo}></img> 
              </div>
              <div className='links'>
-                 <ul>
-                     <li>Nuestros estilos</li>
-                     <li>Noticas</li>
-                     <li>Nosotros</li>
-                     <li>Contacto</li>
-                 </ul>
+                    <Link to ='#'>Nuestros estilos</Link>
+                    <Link>Noticas</Link>
+                    <Link>Nosotros</Link>
+                    <Link>Contacto</Link>                                    
              </div>
          </header>
         </>
