bb6ef389f895725e520891f7b2c032b7846734d4
Pablo Armando <pabloar24.7@hmail.com>
2021-10-04 09:13:52 -0300

    underline with animation

--- my-app/src/components/main/Contacto/Contacto.js
+++ my-app/src/components/main/Contacto/Contacto.js
@@ -3,9 +3,10 @@ import './contacto.css'
 
 function Contacto (){
     return(
-        <div className='container'>
+        <div className='contactContainer'>
             <div className='wrapper'>
                 <h2>Contacto</h2>
+                <div className='underline'></div>
                 <h4>¡Ponete en contacto con nosotros!  <a href="mailto:avispahomebrew@gmail.com">avispahomebrew@gmail.com</a></h4>
                 
 
