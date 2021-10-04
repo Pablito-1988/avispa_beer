c86f09f7b4873f6bf569f0f88343c089cf48b2aa
Pablo Armando <pabloar24.7@hmail.com>
2021-10-03 23:46:17 -0300

    contact first steps

--- my-app/src/components/main/Contacto/Contacto.js
+++ my-app/src/components/main/Contacto/Contacto.js
@@ -4,8 +4,11 @@ import './contacto.css'
 function Contacto (){
     return(
         <div className='container'>
-            <div className='title'>
+            <div className='wrapper'>
                 <h2>Contacto</h2>
+                <h4>¡Ponete en contacto con nosotros!  <a href="mailto:avispahomebrew@gmail.com">avispahomebrew@gmail.com</a></h4>
+                
+
             </div>
 
         </div>
