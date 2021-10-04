a53cd1f0051aafc5ca07f66c3024846d49e23d17
Pablo Armando <pabloar24.7@hmail.com>
2021-10-03 22:41:07 -0300

    login with background

--- my-app/src/components/login/login.js
+++ my-app/src/components/login/login.js
@@ -3,8 +3,7 @@ import './login.css'
 function Access() {
     return(
         <div className='formContainer'>
-            <h4>Hola</h4>
-            <p>hola hola hola hola hola hola</p>
+            
         </div>    
     )
 }
