c535eaa79729f60cc0f37a302b751cc6a33223b0
Pablo Armando <pabloar24.7@hmail.com>
2021-10-04 23:03:45 -0300

    some changes on styles page

--- my-app/src/components/main/Estilos/Estilos.js
+++ my-app/src/components/main/Estilos/Estilos.js
@@ -4,7 +4,25 @@ function Estilos (){
     return(
         <>
             <div className='stylesContainer'>
-                
+                <div className='stylesWrapper'>
+                    <div className='style'>
+                        <h5>soy un estiylo1
+                        </h5>
+                    </div>
+                    <div className='style'>
+                        <h5>soy otro estiylo2
+                        </h5>
+                    </div>
+                    <div className='style'>
+                        <h5>soy otro estiylo3
+                        </h5>
+                    </div>
+                    <div className='style'>
+                        <h5>soy otro estiylo3
+                        </h5>
+                    </div>
+                    
+                </div>
             </div>
         </>
     )
